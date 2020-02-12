82ab80e18d4324bd44843f3dd43d301420a94b9d
David C <dcrosby2718@gmail.com>
2020-02-12 12:14:59 -0800

    update, deploy initial

---
+++ README.md
@@ -0,0 +1,7 @@
+https://reddit-watch.now.sh/
+
+TODO:
+- Persistence
+- Infinite Scrolling
+- Search
+- Dates, only show data within time period
--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ class Viz extends React.Component {
   }
 
   async startUpdates() {
-    let ret = await fetch('http://localhost:3000/api/update');
+    let ret = await fetch(`./api/update`);
     const rett = await ret.json();
     console.log(rett);
     this.setState({
@@ -25,7 +25,8 @@ class Viz extends React.Component {
 
   componentDidMount() {
     this.setState({
-      isMounted: true
+      isMounted: true,
+      width: window.innerWidth
     }, () => this.startUpdates())
   }
 
@@ -34,7 +35,8 @@ class Viz extends React.Component {
       isMounted: false
     })
   }
-  drawChart() {
+
+  getCountsAndKeys() {
     let keys = Object.keys(this.state.subredditPostCount);
     let pairs = Array(keys.length);
     for(let i=0; i < pairs.length; i++) {
@@ -48,11 +50,15 @@ class Viz extends React.Component {
       keys.push  ( pairs[p][0] );
       counts.push( pairs[p][1] );
     }
+    return [keys, counts];
+  }
+  drawChart() {
+    let [keys, counts] = this.getCountsAndKeys();
+    const width = this.state.width;
+    const widthPerPost = width/(3*d3.max(counts));
     const barHeight = 30;
-    const widthPerPost = 20;
-    const yOffset = 30;
-    const width = 800;
     const height = keys.length * barHeight;
+    const yOffset = 30;
 
     const el = new Element('div');
     const svg = d3.select(el)
@@ -60,30 +66,58 @@ class Viz extends React.Component {
       .attr('id', 'chart')
       .attr('width', width)
       .attr('height', height)
-    svg.selectAll("rect")
+    svg
+      .append('g')
+      .selectAll('.bar1')
       .data(counts)
       .enter()
-      .append("rect")
-      .attr("x", 0)
-      .attr("y", (d, i) => i * yOffset)
-      .attr("height", barHeight)
-      .attr("width", (d, i) => d*widthPerPost)
-      .attr("fill", (d, i) => toRGB(keys[i]));
-    svg.selectAll("text")
+      .append('rect')
+      .attr('id', (d, i) => `bar-id-${i}`)
+      .attr('x', this.state.width/5)
+      .attr('y', (d, i) => i * yOffset)
+      .attr('height', barHeight*counts.length)
+      .attr('width', width)
+      .attr('fill', 'none');
+    svg
+      .append('g')
+      .selectAll('.bar2')
+      .data(counts)
+      .enter()
+      .append('rect')
+      .attr('x', this.state.width/5)
+      .attr('y', (d, i) => i * yOffset)
+      .attr('height', barHeight)
+      .attr('width', (d, i) => d*widthPerPost)
+      .attr('fill', (d, i) => toRGB(keys[i]));
+
+    const addOffset = (x, i) => calc(x + 10 + counts[i]*widthPerPost);
+    svg
+      .append('g')
+      .attr('class', 'bar-label')
+      .selectAll('text')
       .data(keys)
       .enter()
-      .append("text")
-      .attr("x", (d, i) => 10 + counts[i]*widthPerPost)
-      .attr("y", (d, i) => i * yOffset + (yOffset/2))
+      .append('text')
       .text((d, i) => {
         return `${d} (${this.state.subredditPostCount[keys[i]]})`
       })
+      .attr('text-align', 'middle')
+      .attr('font', 'helvetica')
+      .attr('x', (d, i) => (this.state.width/5) + 10 + counts[i]*widthPerPost)
+      .attr('y', (d, i) => i * yOffset + (yOffset/1.5))
     return el.toReact();
   }
 
   render() {
+    let [keys, counts] = this.getCountsAndKeys();
+    const msg = `There are ${keys.length} subreddits represented over ${d3.sum(counts)} submissions`;
+    const vizHeaderStyle = {
+      'position': 'relative',
+      'left': '20%'
+    }
     return (
       <div>
+        <h3 style={vizHeaderStyle}>{msg}</h3>
         {this.drawChart()}
       </div>
     )
@@ -99,10 +133,22 @@ export default class extends React.Component {
   }
 
   render() {
+
+    const headerStyle = {
+      'position': 'absolute', 
+      'left': '20%'
+    }
+    const containerStyle = {
+      'position': 'absolute',
+      'top': '10%'
+    }
+
     return (
       <div>
-        <h1>Number of posts on /r/popular/new</h1>
-        <Viz subredditPostCount={this.state.subredditPostCount}/>
+        <h1 style={headerStyle}>Number of posts on /r/popular/new</h1>
+        <div style={containerStyle}>
+          <Viz subredditPostCount={this.state.subredditPostCount}/>
+        </div>
       </div>
     )
   }
